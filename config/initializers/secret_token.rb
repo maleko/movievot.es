3cbe3bf5ed6b447e57ca3bde6b4c0085b3ac2adf
Marc Lee <marc@adviso.com.au>
2013-08-22 23:35:29 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovievotEs::Application.config.secret_key_base = '1e1a035b7e2827f678e95b5afcdf1f003a628fec5d74cf5b430ec4444f8ebbb62e3b5ab47921f10988f5d47c46f9b875204afd0b76f3cdf7228f364236537452'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MovievotEs::Application.config.session_store :cookie_store, key: '_movievot_es_session'
